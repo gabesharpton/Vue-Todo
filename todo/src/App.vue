915c34e12ccb2a25d5c4baca00eea1a28b439b6e
gabesharpton <gabriel.sharpton@gmail.com>
2019-12-01 22:56:09 -0600

    added list component

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <HelloWorld msg="To Do List"/>
+    <List msg="Todo"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import List from './components/List.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    List
   }
 }
 </script>
